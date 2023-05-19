89f430a8e5185f4506a095baca1a50a1e7ab8dde
Rodrigo Leandro <110614877+Rovales@users.noreply.github.com>
2023-05-19 11:56:43 -0300

    Add files via upload

---
+++ login/config.php
@@ -0,0 +1,18 @@
+<?php
+$dbHost = 'Localhost';
+$dbUserName = 'root';
+$dbPassword = '';
+$dbName = 'dp_login';
+
+$conexao = new mysqli($dbHost,$dbUserName,$dbPassword,$dbName );
+
+//if($conexao-> connect_errno)
+//{
+ //   echo "Erro";
+//}
+//else
+//{
+//    echo "Conexão efetuada";
+//}
+
+?>
